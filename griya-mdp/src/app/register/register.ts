38a99ccd09ccc3a5a213dfdd27090802c5a29d33
Clemens Mere Krisna Jemarut <clemensmerekrisnajemarut_2327240055@mhs.mdp.ac.id>
2025-11-24 17:08:32 +0700

    progres angular

--- griya-mdp/src/app/register/register.ts
+++ griya-mdp/src/app/register/register.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormGroup, ReactiveFormsModule, Validator, FormBuilder, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
+  registerForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['',[Validators.required, Validators.minLength(6)]]
+    })
+  }
+
+  submitRegister(): void {
+    if(this.registerForm.valid) {
+      const formData = this.registerForm.value;
+      console.log('Form submited', formData);
+
+      // TODO: Kirim data ke Backend API
+      // this.authService.register(formData).subscribe(...)
+    } else {
+      console.log('form is not valid');
+    }
+  }
 }
